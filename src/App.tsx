ac86120a62e17b329388627cefe6f8effa53014c
MishaKanai <misha.a.kanai@gmail.com>
2021-04-30 20:13:00 +0300

    Export getApp so history can be injected in composition

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { History } from "history";
 import { ConnectedRouter } from "connected-react-router";
 import { Switch, Route, Link, useRouteMatch } from "react-router-dom";
 import { PageRoute } from "./SlateGraph/Page";
@@ -40,40 +41,42 @@ const Docs = React.memo(() => {
     </div>
   );
 });
-
-function App() {
-  return (
-    <div>
-      <ConnectedRouter history={history}>
-        <div>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/docs">Docs</Link>
-            </li>
-          </ul>
-        </div>
-        <div>
-          <Switch>
-            <Route path="/about">
-              <div>About</div>
-            </Route>
-            <Route path="/docs">
-              <Docs />
-            </Route>
-            <Route path="/">
-              <div>Home</div>
-            </Route>
-          </Switch>
-        </div>
-      </ConnectedRouter>
-    </div>
-  );
-}
-
+export const getApp = (history: History<unknown>) => {
+  function App() {
+    return (
+      <div>
+        <ConnectedRouter history={history}>
+          <div>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/docs">Docs</Link>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <Switch>
+              <Route path="/about">
+                <div>About</div>
+              </Route>
+              <Route path="/docs">
+                <Docs />
+              </Route>
+              <Route path="/">
+                <div>Home</div>
+              </Route>
+            </Switch>
+          </div>
+        </ConnectedRouter>
+      </div>
+    );
+  }
+  return App;
+};
+const App = getApp(history);
 export default App;
