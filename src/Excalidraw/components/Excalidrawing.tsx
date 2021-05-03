da3b54468502533fe3bb2b364fa729c1fa76fc37
MishaKanai <misha.a.kanai@gmail.com>
2021-05-03 16:28:06 +0300

    Remove unused Excalidrawing component.

--- src/Excalidraw/components/Excalidrawing.tsx
+++
@@ -1,7 +0,0 @@
-import Draw from "@excalidraw/excalidraw";
-interface ExcalidrawingProps {}
-const Excalidrawing = (props: ExcalidrawingProps) => {
-  return <Draw />;
-};
-
-export default Excalidrawing;
