4deccbd4ff14450de1ca3f44a72cadf7c923678d
MishaKanai <misha.a.kanai@gmail.com>
2021-05-26 02:02:50 +0300

    Fix loop on store subscription by only saving if documents/drawings are different, since we dispatch from within store subscribe, and fix subscription not triggering, because when store is first created, auth flags aren't present (we have to wait for persistor rehydration to complete from redux-persist.)

--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware, compose, Reducer } from "redux";
+import { createStore, applyMiddleware, compose, Reducer, Store } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 import { routerMiddleware } from "connected-react-router";
@@ -9,8 +9,13 @@ import { RootAction } from "./action";
 
 import { DropboxAuth, Dropbox } from "dropbox";
 import parseQueryString from "../dropbox/util/parseQueryString";
-import { authSuccessAction, selectFilePathAction } from "../dropbox/store/actions";
-import debounce from 'lodash/debounce'
+import {
+  authSuccessAction,
+  selectFilePathAction,
+} from "../dropbox/store/actions";
+import debounce from "lodash/debounce";
+import { DrawingDocuments } from "../Excalidraw/store/reducer";
+import { SlateDocuments } from "../SlateGraph/store/reducer";
 // aka app key
 const CLIENT_ID = "9r1uwr2l55chuy7";
 const REDIRECT_URI = window.location.protocol + "//" + window.location.host;
@@ -47,54 +52,100 @@ const persistedReducer: Reducer<RootState & PersistPartial, RootAction> =
 const composeEnhancers =
   (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+const syncDropboxToStore = (
+  accessToken: string,
+  store: Store<RootState & PersistPartial, RootAction>
+) => {
+  console.log("syncDropboxToStore");
+  const dbx = new Dropbox({ accessToken });
+  let prevDocuments: SlateDocuments;
+  let prevDrawings: DrawingDocuments;
+  store.subscribe(
+    debounce(() => {
+      console.log("called");
+      const state = store.getState();
+      if (
+        state.auth.state === "authorized" &&
+        state.auth.selectedFilePath &&
+        state.auth.rev &&
+        // update only on data (otherwise we loop due to state update triggered from below)
+        (state.documents !== prevDocuments || state.drawings !== prevDrawings)
+        // TODO next step: Update only the files necessary to update.
+      ) {
+        console.log("ok uploading");
+        dbx
+          .filesUpload({
+            mode: {
+              ".tag": "update",
+              update: state.auth.rev,
+            },
+            path: state.auth.selectedFilePath, // `/path/to/file-name.json`
+            contents: new File(
+              [
+                JSON.stringify(
+                  {
+                    documents: state.documents,
+                    drawings: state.drawings,
+                  },
+                  null,
+                  2
+                ),
+              ],
+              state.auth.selectedFilePath.slice(1),
+              {
+                type: "application/json",
+              }
+            ),
+            // ...other dropbox args
+          })
+          .then((response) => {
+            prevDocuments = state.documents;
+            prevDrawings = state.drawings;
+            store.dispatch(
+              selectFilePathAction(
+                response.result.path_lower!,
+                response.result.rev
+              )
+            );
+            // on success we can dispatch "upload complete" or something like that
+          });
+      }
+    }, 2000)
+  );
+};
+
 const configureStore = () => {
   let store = createStore(
     persistedReducer,
     compose(composeEnhancers(applyMiddleware(routerMiddleware(history))))
   );
-  let persistor = persistStore(store as any);
-
-  if (hasRedirectedFromAuth()) {
-    // redirect to authed page OR set app to authorized.
-    dbxAuth.setCodeVerifier(window.sessionStorage.getItem("codeVerifier")!);
-    dbxAuth
-      .getAccessTokenFromCode(REDIRECT_URI, getCodeFromUrl())
-      .then((response) => {
-        const accessToken = (response.result as any).access_token;
-        store.dispatch(authSuccessAction(accessToken))
-
-        const dbx =  new Dropbox({ accessToken })
-        store.subscribe(debounce(() => {
-          const state = store.getState();
-          if (state.auth.state === 'authorized' && state.auth.selectedFilePath && state.auth.rev) {
-            dbx
-            .filesUpload({
-              mode: {
-                ".tag": "update",
-                update: state.auth.rev
-              },
-              path: state.auth.selectedFilePath, // `/path/to/file-name.json`
-              contents: new File([JSON.stringify({
-                documents: state.documents,
-                drawings: state.drawings
-              }, null, 2)], state.auth.selectedFilePath.slice(1), {
-                type: "application/json",
-              }),
-              // ...other dropbox args
-            })
-            .then((response) => {
-              console.log(response);
-              store.dispatch(selectFilePathAction(response.result.path_lower!, response.result.rev))
-              // on success we can dispatch "upload complete" or something like that
-            });
-          }
-        
-        }, 5000))
-      })
+  let persistor = persistStore(store as any, undefined, () => {
+    if (hasRedirectedFromAuth()) {
+      // redirect to authed page OR set app to authorized.
+      dbxAuth.setCodeVerifier(window.sessionStorage.getItem("codeVerifier")!);
+      dbxAuth
+        .getAccessTokenFromCode(REDIRECT_URI, getCodeFromUrl())
+        .then((response) => {
+          const accessToken = (response.result as any).access_token;
+          store.dispatch(authSuccessAction(accessToken));
+          syncDropboxToStore(accessToken, store);
+        });
       // .catch((error) => {
       //   console.error(error);
       // });
-  }
+    } else {
+      const initialState = store.getState();
+      console.log({
+        initialState,
+      });
+      const accessToken =
+        initialState.auth.state === "authorized" &&
+        initialState.auth.accessToken;
+      if (accessToken) {
+        syncDropboxToStore(accessToken, store);
+      }
+    }
+  });
   return { store, persistor };
 };
 export default configureStore;
