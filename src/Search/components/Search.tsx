ed865db515b668946da4307c9345fb464a344676
Misha Kanai <misha.a.kanai@gmail.com>
2022-01-09 20:44:01 -0500

    Add highlighting to text search results.

--- src/Search/components/Search.tsx
+++ src/Search/components/Search.tsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
-import { TextField } from '@mui/material';
+import React, { ReactElement, useState } from 'react';
+import { TextField, useTheme } from '@mui/material';
 import { usePhraseSearch } from '../hooks/useSearch';
 
 const Search: React.FC<{}> = () => {
     const [inputText, setInputText] = useState('')
     const results = usePhraseSearch(inputText.toLowerCase().trim())
+    const theme = useTheme()
     return <div>
         <div>
             <TextField label="Search" size="small" value={inputText} onChange={e => setInputText(e.target.value)} />
@@ -15,8 +16,16 @@ const Search: React.FC<{}> = () => {
                     <b>{docName}</b>
                     <ul>
                         {Array.from(matches).map((text, i) => <li key={i}>{
-                            // TODO: highlight searched phrase.
-                           text
+                            (() => {
+                                const matches = text.match(new RegExp(inputText, 'i')) ?? [];
+
+                                return text.split(new RegExp(inputText, 'i')).reduce((prev, curr, i) => {
+                                    const highlightColor = theme.palette.secondary.light;
+                                    prev.push(<span key={i}>{curr}</span>);
+                                    prev.push(<span key={i + ':match'} style={{ backgroundColor: highlightColor, color: theme.palette.getContrastText(highlightColor) }}>{matches[i]}</span>)
+                                    return prev;
+                                }, [] as ReactElement[])
+                            })()
                         }</li>)}
                     </ul>
                 </li>
