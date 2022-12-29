04afca3d63c9d5157de108f8ff191ba57c460cd5
Misha Kanai <misha.a.kanai@gmail.com>
2022-12-29 06:09:02 -0500

    Go back to sessionStorage for uploadedFiles. TODO explore other large file handling - probably make storing files locally opt-in, and default to storing in dbx or whatever.

--- src/store/createRootReducer.ts
+++ src/store/createRootReducer.ts
@@ -11,33 +11,7 @@ import uploadedFilesReducer from '../UploadedFiles/uploadedFilesSlice';
 import {
   persistReducer
 } from 'redux-persist';
-import localforage from 'localforage';
-
-/**
- * Each session gets its own IDB key/value entry, to store files in.
- * When the session 'ends' (i.e. beforeunload) we delete the entry.
- */
-const TAB_ID_KEY = 'tabId';
-const getTabId = () => Date.now() + ':' + (Math.random() * 100000).toFixed(0);
-
-const tabId = (() => {
-  const storedTabId = sessionStorage.getItem(TAB_ID_KEY);
-  if (storedTabId) {
-    return storedTabId;
-  }
-  const newTabId = getTabId();
-  sessionStorage.setItem(TAB_ID_KEY, newTabId);
-  return newTabId;
-})();
-
-// by prepending 'persist:' we get the real key name in the IDB object store.
-const getKVName = (key: string, prependPersist: boolean) =>  (prependPersist ? 'persist:' : '') + tabId + ':' + key
-
-const handleUnload = () => {
-  localforage.removeItem(getKVName('uploadedFiles', true))
-};
-window.addEventListener('beforeunload', handleUnload)
-
+import storageSession from "redux-persist/lib/storage/session";
 
 const createRootReducer = (history: History) =>
   combineReducers({
@@ -49,8 +23,8 @@ const createRootReducer = (history: History) =>
     recentlyOpened: recentlyOpenedReducer,
     collections: collectionsReducer,
     files: persistReducer({
-      key: getKVName('uploadedFiles', false),
-      storage: localforage
+      key: 'uploadedFiles',
+      storage: storageSession
     }, combineReducers({
       uploadedFiles: uploadedFilesReducer
     }))
